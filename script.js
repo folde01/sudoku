c50b14e00c0ebc78f6ef29b019afe87f10226a8f
JO <5667502+folde01@users.noreply.github.com>
2019-03-07 19:06:29 +0000

    One cell can be active at a time. Input table has a different style when active.

--- script.js
+++ script.js
@@ -24,24 +24,28 @@ inputCells.forEach(function (cell, index) {
     }
 });
 
+var activeCellIndex = null;
+const inputTable = document.querySelector('.inputTable');
+
 cells.forEach(function (cell, cellIndex) {
     if (cellValues[cellIndex] === 0) {
         cell.innerHTML = '';
-        cell.addEventListener('click', function() {
-            // console.log('cell ' + cellIndex + ' clicked');
-            cell.style.backgroundColor = 'white';
-            inputCells.forEach(function(inputCell, inputCellIndex){
-                inputCell.style.backgroundColor = 'white';
+        cell.addEventListener('click', function () {
+            if (activeCellIndex) {
+                cells[activeCellIndex].classList.remove('activeCell');
+            }
+            activeCellIndex = cellIndex;
+            cells[activeCellIndex].classList.add('activeCell');
+            inputTable.classList.add('inputTableActive');
+            inputCells.forEach(function (inputCell, inputCellIndex) {
                 // Use onClick instead of addEventListener here as we need to replace a handler, not add one:
-                inputCell.onclick = function(event){
-                    // console.log('inputCell ' + inputCell.innerHTML + ' clicked');
+                inputCell.onclick = function () {
                     cell.innerHTML = inputCell.innerHTML;
-                    // console.log('cell ' + cellIndex + ' parent: ' + cell.parentNode)
+                    cell.classList.remove('activeCell');
+                    inputTable.classList.remove('inputTableActive');
                 };
-                // console.log('inputCell ' + inputCellIndex + ' listening');
             });
         });
-        // console.log('cell listening: ' + cellIndex);
     } else {
         cell.innerHTML = cellValues[cellIndex];
         cell.style.color = 'grey';
