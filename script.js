060f29abc7ffc07fe799d1e4a762c09944540d21
JO <5667502+folde01@users.noreply.github.com>
2019-03-09 22:16:01 +0000

    Add moveIsValid(), which so far checks the validity of a move made in the first row.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Easy Puzzle 8,835,213,384 from websudoku.com
 const cellValues = [
     0, 5, 0, 0, 0, 1, 0, 0, 6,
@@ -15,7 +13,6 @@ const cellValues = [
 
 const cells = document.querySelectorAll('.cell');
 
-
 const inputCells = document.querySelectorAll('.inputCell');
 
 inputCells.forEach(function (cell, index) {
@@ -24,34 +21,40 @@ inputCells.forEach(function (cell, index) {
     }
 });
 
-
-function moveIsInvalid(){
-    return false;
-}
-
-
 var activeCellIndex = null;
 const inputTable = document.querySelector('.inputTable');
 
 cells.forEach(function (cell, cellIndex) {
+
     if (cellValues[cellIndex] === 0) {
         cell.classList.add('inactiveCell');
         cell.innerHTML = '';
         cell.addEventListener('click', function () {
+
             if (activeCellIndex) {
                 cells[activeCellIndex].classList.remove('activeCell');
             }
+
             activeCellIndex = cellIndex;
             cells[activeCellIndex].classList.add('activeCell');
             inputTable.classList.add('inputTableActive');
             inputCells.forEach(function (inputCell, inputCellIndex) {
                 // Use onClick instead of addEventListener here as we need to replace a handler, not add one:
+                const moveValue = inputCell.innerText;
                 inputCell.onclick = function () {
-                    cell.innerHTML = inputCell.innerHTML;
+                    if (!moveIsValid(cellIndex, moveValue)){
+                        console.log('INVALID');
+                        cell.classList.add('invalidMove');
+                    } else {
+                        cell.classList.remove('invalidMove');
+                    }
+                    cell.innerHTML = moveValue;
                     cell.classList.remove('activeCell');
+
+
                     inputTable.classList.remove('inputTableActive');
-                    inputCells.forEach(function(inputCell, inputCellIndex) {
-                        inputCell.onclick = function () { return false };
+                    inputCells.forEach(function (inputCell, inputCellIndex) {
+                        inputCell.onclick = function () { return false; };
                     });
                 };
             });
@@ -60,4 +63,45 @@ cells.forEach(function (cell, cellIndex) {
         cell.innerHTML = cellValues[cellIndex];
         cell.classList.add('clueCell');
     }
+
 });
+
+
+var hintsEnabled = false;
+const hintsButton = document.querySelector('.hintsButton');
+
+// what if you enable hints part-way through. Do you see past invalid moves?
+hintsButton.addEventListener('click', function () {
+    hintsEnabled = !hintsEnabled;
+
+    if (hintsEnabled) {
+        hintsButton.innerHTML = 'Disable hints';
+    } else {
+        hintsButton.innerHTML = 'Enable hints';
+    }
+
+});
+
+
+function moveIsValid(cellIndex, moveValue){
+    return rowIsValid(cellIndex, moveValue) &&
+        columnIsValid(cellIndex, moveValue) && squareIsValid(cellIndex, moveValue);
+}
+
+function rowIsValid(cellIndex, moveValue){
+    const cellValues = Array.from(cells).map((cell) => { return cell.innerText; });
+    const firstCellToCheck = 0;
+    const lastCellToCheck = 9; 
+    const cellValuesToCheck = cellValues.slice(firstCellToCheck, lastCellToCheck).filter((cellValue) => {
+        return cellValue !== '';
+    });
+
+    return !cellValuesToCheck.includes(moveValue);
+}
+
+function columnIsValid(cellIndex){
+    return true;
+}
+function squareIsValid(cellIndex){
+    return true;
+}
