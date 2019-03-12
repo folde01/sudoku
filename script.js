91862675efdc135ad9c009c0ff29aaeeea5e089f
JO <5667502+folde01@users.noreply.github.com>
2019-03-12 19:42:12 +0000

    Move cellValues2D array out of moveIsValid() so it is initialized once but updated on each move.

--- script.js
+++ script.js
@@ -13,48 +13,96 @@ const cellValues = [
 
 const cells = document.querySelectorAll('.cell');
 
+// Build 2D array used to check move validity
+let cellValues2D = new Array(9);
+
+for (let i = 0; i < cellValues2D.length; i++) {
+    cellValues2D[i] = new Array(9);
+}
+
+for (let i = 0; i < cells.length; i++) {
+    // console.log('cell: ' + cells[i].innerText);
+    const x = i % 9;
+    const y = Math.floor(i / 9);
+    cellValues2D[x][y] = cells[i].innerText;
+}
+
+// Populate cells (DOM cell nodes) and cellValues2D (for checking move validity) arrays with values from cellValues. 
+cells.forEach(function (cell, cellIndex) {
+    const cellX = cellIndex % 9;
+    const cellY = Math.floor(cellIndex / 9);
+    let cellValue = null;
+
+    if (cellValues[cellIndex] === 0) {
+        // It's an empty cell.
+        cellValue = '';
+    } else {
+        // It's a clue cell.
+        cellValue = cellValues[cellIndex].toString();
+        cell.classList.add('clueCell');
+    }
+    cell.innerText = cellValue;
+    cellValues2D[cellX][cellY] = cellValue;
+});
+
+// console.log('INIT cellValues2D: ' + cellValues2D);
+
+// Set up keypad
 const inputCells = document.querySelectorAll('.inputCell');
 
 inputCells.forEach(function (cell, index) {
     if (index < inputCells.length - 1) {
-        cell.innerHTML = index + 1;
+        cell.innerText = (index + 1).toString();
     }
 });
-
-var activeCellIndex = null;
 const inputTable = document.querySelector('.inputTable');
 
 
+// Used to ensure one cell is active (selected) at a time
+let activeCellIndex = null;
 
-
+// Add event listeners to all cells except clue cells.
 cells.forEach(function (cell, cellIndex) {
-
+    // Non-clue cells
     if (cellValues[cellIndex] === 0) {
-        cell.classList.add('inactiveCell');
-        cell.innerHTML = '';
+
         cell.addEventListener('click', function () {
 
-            if (activeCellIndex) {
+            // Activate cell. Only one cell should be active at a time.
+            if (activeCellIndex !== null) {
                 cells[activeCellIndex].classList.remove('activeCell');
             }
-
             activeCellIndex = cellIndex;
             cells[activeCellIndex].classList.add('activeCell');
+
+            // Activate keypad.
             inputTable.classList.add('inputTableActive');
             inputCells.forEach(function (inputCell, inputCellIndex) {
-                // Use onClick instead of addEventListener here as we need to replace a handler, not add one:
                 const moveValue = inputCell.innerText;
+
+                // Use onClick instead of addEventListener as we need to replace a handler, not add one.
                 inputCell.onclick = function () {
                     if (!moveIsValid(cellIndex, moveValue)) {
-                        console.log('INVALID');
+                        console.log('INVALID MOVE');
+
+                        // Show that the move breaks the sudoku rules.
                         cell.classList.add('invalidMove');
                     } else {
                         cell.classList.remove('invalidMove');
                     }
-                    cell.innerHTML = moveValue;
+
+                    // Set cell value in DOM
+                    cell.innerText = moveValue;
+
+                    // Set cell value in 2D array used to check move validity
+                    const cellX = cellIndex % 9;
+                    const cellY = Math.floor(cellIndex / 9);
+                    cellValues2D[cellX][cellY] = moveValue;
+
+                    // Deactivate cell 
                     cell.classList.remove('activeCell');
 
-
+                    // Deactivate input keypad
                     inputTable.classList.remove('inputTableActive');
                     inputCells.forEach(function (inputCell, inputCellIndex) {
                         inputCell.onclick = function () { return false; };
@@ -63,10 +111,10 @@ cells.forEach(function (cell, cellIndex) {
             });
         });
     } else {
-        cell.innerHTML = cellValues[cellIndex];
+        // Clue cell
+        cell.innerText = cellValues[cellIndex];
         cell.classList.add('clueCell');
     }
-
 });
 
 
@@ -78,9 +126,9 @@ hintsButton.addEventListener('click', function () {
     hintsEnabled = !hintsEnabled;
 
     if (hintsEnabled) {
-        hintsButton.innerHTML = 'Disable hints';
+        hintsButton.innerText = 'Disable hints';
     } else {
-        hintsButton.innerHTML = 'Enable hints';
+        hintsButton.innerText = 'Enable hints';
     }
 
 });
@@ -91,17 +139,7 @@ function moveIsValid(cellIndex, moveValue) {
     const cellX = cellIndex % 9;
     const cellY = Math.floor(cellIndex / 9);
 
-    const cellValues2D = new Array(9);
-
-    for (let i = 0; i < cellValues2D.length; i++) {
-        cellValues2D[i] = new Array(9);
-    }
-
-    for (let i = 0; i < cells.length; i++) {
-        const x = i % 9;
-        const y = Math.floor(i / 9);
-        cellValues2D[x][y] = cells[i].innerText;
-    }
+    // console.log('cellValues2D: ' + cellValues2D);
 
     function rowIsValid() {
         console.log('rowIsValid?');
@@ -109,6 +147,8 @@ function moveIsValid(cellIndex, moveValue) {
         let result = true;
 
         for (let i = 0; i < 9; i++) {
+            // console.log([cellValues2D[i][cellY], moveValue].join());
+            // console.log([typeof(cellValues2D[i][cellY]), typeof(moveValue)].join());
             if (cellValues2D[i][cellY] !== '' && cellValues2D[i][cellY] === moveValue) {
                 result = false;
             }
@@ -133,7 +173,7 @@ function moveIsValid(cellIndex, moveValue) {
     }
 
     function boxIsValid() {
-        console.log('squareIsValid?');
+        console.log('boxIsValid?');
 
         let result = true;
 
